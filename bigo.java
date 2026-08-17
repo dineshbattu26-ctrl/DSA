70178600ffb633dcad0da115348f4540bc6fc254
Dinesh <dineshbattu26@gmail.com>
2026-08-17 21:30:25 +0530

    first commit

---
+++ bigo.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void printitems(int n){
+        for(int i=0;i<n;i++){
+            System.out.println(i);
+        }
+    }
+
+
+    public static void main(String[] args){
+        printitems(10);
+    }
+}
+    
+    
